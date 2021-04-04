e76d65d4a9e3af7846ba4ae83d89b995dea68138
Ansh Srivastava <ansh_srtv@outlook.com>
2021-04-04 20:46:41 +0530

    fix: logging

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -67,6 +67,7 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
+            app.logger.info('Invalid login attempt')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
@@ -91,7 +92,6 @@ def authorized():
             redirect_uri=url_for('authorized', _external=True, _scheme='https')
         )
         if "error" in result:
-            app.logger.info('Invalid login attempt')
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
         # Note: In a real app, we'd use the 'name' property from session["user"] below
